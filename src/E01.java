1f3d5e021f461676300b585e81afa877089f7359
Oğuz Karaca <98693521+Oguzkrc@users.noreply.github.com>
2022-11-02 22:48:51 +0300

    deneme branch'inde e01'e ekleme yaptim

--- src/E01.java
+++ src/E01.java
@@ -3,5 +3,8 @@ import com.sun.tools.javac.Main;
 public class E01 {
     public static void main(String[] args) {
 
+
+        System.out.println("deneme branch'inde degisiklik yaptik");
+
     }
 }
