be095fb99c38901d56ef23943ba0a66879943a6d
Oğuz Karaca <98693521+Oguzkrc@users.noreply.github.com>
2022-11-02 22:56:17 +0300

    branch'ler birlestirildi

--- src/E01.java
+++ src/E01.java
@@ -2,6 +2,6 @@ import com.sun.tools.javac.Main;
 
 public class E01 {
     public static void main(String[] args) {
-
+        System.out.println("main branch'de e01 de degisiklik yaptim");
     }
 }
