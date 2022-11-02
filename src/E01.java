f0443edc62fdadb4857db2d3ccb6b890b9d8acea
Oğuz Karaca <98693521+Oguzkrc@users.noreply.github.com>
2022-11-02 23:01:03 +0300

    e01 main branch degisiklik

--- src/E01.java
+++ src/E01.java
@@ -3,5 +3,8 @@ import com.sun.tools.javac.Main;
 public class E01 {
     public static void main(String[] args) {
         System.out.println("main branch'de e01 de degisiklik yaptim");
+
+
+        System.out.println("main branch e01 9.satirda degisiklik yaptim");
     }
 }
